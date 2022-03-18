4031e446de494fb1021c2b8c8752e0f2813b7972
Ubuntu <devopsadmin@apcom-vm-blog-azdb-prod-eastus-1.qlc2fv1d3whexnd32bhzv544ra.bx.internal.cloudapp.net>
2022-03-18 06:30:15 +0000

    init

---
+++ kernelupdate/family5-64.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt dist-upgrade -y
+
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.15/amd64/linux-headers-5.16.15-051615-generic_5.16.15-051615.202203161444_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.15/amd64/linux-headers-5.16.15-051615_5.16.15-051615.202203161444_all.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.15/amd64/linux-image-unsigned-5.16.15-051615-generic_5.16.15-051615.202203161444_amd64.deb
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.16.15/amd64/linux-modules-5.16.15-051615-generic_5.16.15-051615.202203161444_amd64.deb
+
+sudo dpkg -i *.deb
